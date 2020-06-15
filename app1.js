2b8d1c4b992e3d4ca5de6972b217ea4d77caf428
mbasitali <mbasitali.cs@gmail.cm>
2020-06-15 10:53:25 -0700

    JavaScript_Assignment

---
+++ app1.js
@@ -0,0 +1,347 @@
+
+//1
+// alert("WelCome  to visit my page ");
+
+// var a = prompt("Enter Password","Your value is");
+// var b = "123";
+// if(a == b)
+// {
+//     alert("Welcome to JS Land..\n Happy Coding!");
+//     alert("Happy Coding!");
+//     alert("Hello.. I can run JS through my web browser's console");
+    
+// }
+// else
+// {
+//     alert("Error! :Please enter valid password");
+// }
+
+//1 end 
+
+
+//2
+//----------------------
+// var a = prompt("Enter UserName","UserName");
+// document.write(a);
+
+//------------------
+// var a= "Basit";
+// var b= "Ali";
+// var c= a + b;
+// document.write(c);
+
+//------------------
+// var a= "Hello World";
+// alert(a);
+
+//----------
+// var a= "Basit";
+// var b= "25 year old ";
+// var c= "Certified Mobile Application Developer";
+// alert(a);
+// alert(b);
+// alert(c);
+
+//-------------
+// var a= "Pizza";
+//   a = --a;
+//   a=a;
+//   alert(a);
+
+//------------------
+// var a= "mbasitali.cs@gmail.com";
+// var b = "My emai Address is ";
+//   alert(b+a);
+
+//-----------
+// var book= "I am trying to learn from the Book A smarter way to learn javascript";
+//   alert(book);
+
+
+//----------------------
+// var a = "Yah! Ican crite HTML Content throught javascript";
+// document.write(a);
+
+
+//3
+//----------------------
+
+// var age = 15;
+// var message = "I am"+age+" year old";
+// alert(message);
+
+
+//---------
+// var time = 14;
+// var message = "you have visited this site"+time+" time";
+// alert(message);
+
+//-----------
+// var year= 1995;
+// document.write("My Birth year is "+year);
+
+//-----------------
+
+// var  visitorname = "basit";
+// var productTite = "laptop";
+// var Quantity = 15;
+// document.write(visitorname +" Order "+Quantity +"   "+ productTite +" on ABC Company...");
+
+//4
+ //var x,y,z;
+
+
+
+
+
+
+
+
+
+
+
+
+//5
+// var a = 5;
+// var b = 3;
+// var c = a + b;
+// document.write(a +" + " + b + "=" + c +"</br>"); 
+
+// var a = 5;
+// var b = 3;
+// var c = a - b;
+// document.write(a +" - " + b + "=" + c +"</br>"); 
+
+// var a = 5;
+// var b = 3;
+// var c = a * b;
+// document.write(a +" * " + b + "=" + c +"</br>"); 
+
+
+// var a = 5;
+// var b = 3;
+// var c = a % b;
+// document.write(a +" % " + b + "=" + c +"</br>"); 
+
+
+//-----------------
+// document.write("Value after variable declaretion is undefined </br>");
+// var a = 5
+// document.write("Initial vaue:"+ a);
+// var b = ++a;
+// document.write("Value after increment is: "+b+"</br>");
+// var c = 7+ b;
+// document.write("Value after Addition is: "+ c +"</br>");
+// var d = --c ;
+// document.write("Value after decrement is: "+d +"</br>");
+// var e = d %  3;
+// document.write("the remainder is "+e);
+
+
+//--------------------
+// document.write("Cost Move Ticket is 600 PKR </br>");
+// document.write("Buying  Ticket is 5 </br>");
+// var a = 600 * 5;
+// document.write("Total cost to buy 5 tickets to a moves is: "+ a +" PKR");
+
+//------------
+// var a = prompt("Enter Tabe","Tabe name");
+// document.write("Tabe of "+ a + "</br>");
+// document.write(a +"* 1 = "+ a * 1 + "</br>");
+// document.write(a +"* 2 = "+ a * 2 + "</br>");
+// document.write(a +"* 3 = "+ a * 3 + "</br>");
+// document.write(a +"* 4 = "+ a * 4 + "</br>");
+// document.write(a +"* 5 = "+ a * 5 + "</br>");
+// document.write(a +"* 6 = "+ a * 6 + "</br>");
+// document.write(a +"* 7 = "+ a * 7 + "</br>");
+// document.write(a +"* 8 = "+ a * 8 + "</br>");
+// document.write(a +"* 9= "+ a * 9 + "</br>");
+// document.write(a +"* 10 = "+ a * 10 + "</br>");
+
+
+//------------
+// var item1 = 650;
+// var Quantitem1 = 3;
+// document.write("Price of item 1 is: " +item1 + "</br>");
+// document.write("Quantity of item 1 is: " +Quantitem1 + "</br>");
+// var item2 = 100;
+// var Quantitem2 = 7;
+// document.write("Quantity of item 2 is: " +Quantitem2 + "</br>");
+// var ShippingCharges = 100;
+// document.write("Shipping Charges is: " +ShippingCharges + "</br>");
+// var Total = (item1*3)+(item2*7)+100;
+// document.write("Total cost of your order is: " +Total + "</br>");
+
+//---------------------------
+// var marks = 980;
+// var obtainmarks = 804;
+// var Percentage = (obtainmarks * 100)/marks;
+// document.write("Total marks :"+marks+ "</br>");
+// document.write("Marks Obtained :"+obtainmarks+ "</br>");
+// document.write("Percentage is: " +Percentage + "</br>");
+
+//-----------------------
+// var us = 10 * 104.80;
+// var saudi = 25 * 28;
+// var result = us + saudi; 
+// document.write("Currency in PKR </br>");
+// document.write("Total Currency in PKR : " +result + "</br>");
+
+
+//--------------------------
+// var radius =20;
+// var circum = 2 * radius * 3.142;
+// var area = 3.142 * (radius*radius)
+// document.write("The Circumference is :"+circum+"</br>");
+// document.write("The area is : " +area + "</br>");
+
+//-------------------------------------
+
+
+
+//6-9
+//-------------------------------------------------
+// var a = 10;
+// document.write("The result of a is : " + a + "</br>");
+// document.write("--------------------------------------</br>");
+
+// a = ++a;
+// document.write("The value of ++a is : " +a + "</br>");
+// document.write("Now the value of a is : " +a + "</br>");
+// document.write("--------------------------------------</br>");
+
+
+// document.write("The value of a is : " +a++ + "</br>");
+// document.write("The value of a++ is : " + a + "</br>");
+// document.write("--------------------------------------</br>");
+
+
+// document.write("The value of --a is : " + --a + "</br>");
+// document.write("The value of --a is : " + a + "</br>");
+// document.write("--------------------------------------</br>");
+
+
+
+// document.write("The value of a is : " +a-- + "</br>");
+// document.write("The value of a++ is : " + a + "</br>");
+// document.write("--------------------------------------</br>");
+
+
+//-------------------------------------------------
+// var a = 2;
+// var b= 1;
+// var result;
+ 
+// a = --a;
+// a = --a - --b;
+// b = --a - --b + ++b;
+// result = --a - --b + ++b + b--;
+
+// document.write("The value of a is : " + a + "</br>");
+// document.write("--------------------------------------</br>");
+// document.write("The value of b is : " + b + "</br>");
+// document.write("--------------------------------------</br>");
+// document.write("The value of result is : " + result + "</br>");
+// document.write("--------------------------------------</br>");
+
+
+
+//----------------------------------------
+// var a = prompt("Enter Tabe","5");
+// if(a != 5){
+// document.write("Tabe of "+ a + "</br>");
+// document.write(a +"* 1 = "+ a * 1 + "</br>");
+// document.write(a +"* 2 = "+ a * 2 + "</br>");
+// document.write(a +"* 3 = "+ a * 3 + "</br>");
+// document.write(a +"* 4 = "+ a * 4 + "</br>");
+// document.write(a +"* 5 = "+ a * 5 + "</br>");
+// document.write(a +"* 6 = "+ a * 6 + "</br>");
+// document.write(a +"* 7 = "+ a * 7 + "</br>");
+// document.write(a +"* 8 = "+ a * 8 + "</br>");
+// document.write(a +"* 9= "+ a * 9 + "</br>");
+// document.write(a +"* 10 = "+ a * 10 + "</br>");
+// }
+// else{
+    
+//         document.write("Tabe of "+ a + "</br>");
+// document.write(a +"* 1 = "+ a * 1 + "</br>");
+// document.write(a +"* 2 = "+ a * 2 + "</br>");
+// document.write(a +"* 3 = "+ a * 3 + "</br>");
+// document.write(a +"* 4 = "+ a * 4 + "</br>");
+// document.write(a +"* 5 = "+ a * 5 + "</br>");
+// document.write(a +"* 6 = "+ a * 6 + "</br>");
+// document.write(a +"* 7 = "+ a * 7 + "</br>");
+// document.write(a +"* 8 = "+ a * 8 + "</br>");
+// document.write(a +"* 9= "+ a * 9 + "</br>");
+// document.write(a +"* 10 = "+ a * 10 + "</br>");
+// }
+
+
+//---------------------------------------------
+
+
+// var a = prompt("Enter Subject","Subject name");
+// var b = +prompt("Enter Subject Obtained mark","Mark");
+
+// var c = prompt("Enter Subject","Subject name");
+// var d = +prompt("Enter Subject Obtained mark","Mark");
+
+// var e = prompt("Enter Subject","Subject name");
+// var f = +prompt("Enter Subject Obtained mark","Mark");
+
+// var z = b + d + f ;
+
+// var SubjectTotal = 100;
+// document.write("SubjectTotal" +"       "+ TotalMark  + "    " + "Obtained Mark" +"             " + " Percentage"+"</br>");
+// document.write(a +"       "+ SubjectTotal  + "    " + b +"             " + b +"%</br>");
+// document.write(c +"       "+ SubjectTotal  + "    " + d +"             " + d +"%</br>");
+// document.write(e +"       "+ SubjectTotal  + "    " + f +"             " + f +"%</br>");
+
+// document.write("              "+ (SubjectTotal*3)+"   " + z+ "       " + z * SubjectTotal / (SubjectTotal*3)+"%</br>");
+
+
+
+
+
+
+//9-11
+
+//------------------------------------------
+// var a = prompt("Enter Gender","Gender");
+// if(a == "male")
+// {
+// document.write( "Good Morning Sir");
+// }
+// else if(a == "female")
+// {
+//     document.write(" Good Morning Ma'am");   
+// }
+
+
+//---------------------------------------
+// var a = prompt("Enter City","CityName");
+// if(a == "Karachi")
+// {
+// document.write( " welcome the user like this: “Welcome to city of lights”");
+// }
+
+
+
+//---------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
